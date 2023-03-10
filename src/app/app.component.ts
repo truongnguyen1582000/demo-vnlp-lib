9a37ba11f247176950a95b544878a4112df7c1c8
truongnguyen1582000 <truongnguyen1582000>
2023-03-10 17:18:39 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,132 @@
 import { Component } from '@angular/core';
+import { DatepickerOptions } from '@em-and-ai/ui-sdk/vnlp-calendar/src/models/vnlp-calendar.model';
+import locale from 'date-fns/locale/vi';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  date: Date = new Date();
+  options: DatepickerOptions = {
+    locale: locale,
+    selectRange: false,
+    placeholder: 'dd/mm/yyyy',
+  };
+  demoChecked = false;
+  demoCheckboxData = [
+    { title: 'option 1', checked: false },
+    { title: 'option 2', checked: true },
+    { title: 'option 3', checked: false },
+    { title: 'option 4', checked: false },
+    { title: 'option 5', checked: true },
+  ];
+  demoSwitch = false;
+  demoInputValue = '';
+
+  dataSource = [
+    {
+      value: 'hello',
+      entity_type: 'greeting',
+      intent: 'greet',
+    },
+    {
+      value: 'how are you',
+      entity_type: 'question',
+      intent: 'ask',
+    },
+    {
+      value: 'goodbye',
+      entity_type: 'farewell',
+      intent: 'farewell',
+    },
+    {
+      value: 'buy',
+      entity_type: 'action',
+      intent: 'order',
+    },
+    {
+      value: 'cancel',
+      entity_type: 'action',
+      intent: 'cancel',
+    },
+    {
+      value: 'pizza',
+      entity_type: 'food',
+      intent: 'order',
+    },
+    {
+      value: 'hamburger',
+      entity_type: 'food',
+      intent: 'order',
+    },
+    {
+      value: 'water',
+      entity_type: 'drink',
+      intent: 'order',
+    },
+    {
+      value: 'coke',
+      entity_type: 'drink',
+      intent: 'order',
+    },
+  ];
+
+  columns = [
+    {
+      title: 'Name',
+      key: 'value',
+      sortable: true,
+    },
+    {
+      title: 'Entity type',
+      key: 'entity_type',
+      sortable: true,
+    },
+    {
+      title: 'Intent',
+      key: 'intent',
+      sortable: false,
+    },
+  ];
+
+  switcherList = [
+    { title: 'Section 1', icon: 'design-grid-filled', key: 'key1' },
+    { title: 'Section 2', icon: 'pen-line', key: 'key2' },
+  ];
+
+  handleBindingDataChange(data: any) {
+    console.log(data);
+  }
+
+  handleBindingDataTableChange(data: any) {
+    console.log(data);
+  }
+  listData = [
+    { id: 1, value: 'Codecademy' },
+    { id: 2, value: 'Coursera' },
+    { id: 3, value: 'Khan Academy' },
+    { id: 4, value: 'LinkedIn Learning' },
+    { id: 5, value: 'Open Culture' },
+    { id: 6, value: 'Sophia' },
+    { id: 7, value: 'Teacher Training Videos' },
+    { id: 8, value: 'Udemy' },
+    { id: 9, value: 'Virtual Nerd Mobile Math' },
+    { id: 10, value: 'Techsmith' },
+  ];
+
+  dropdownSettings = {
+    singleSelection: false,
+    allowSearchFilter: true,
+    placeholder: 'Placeholder',
+    dropdownWidth: '321px',
+  };
+
+  getDropdownValue(selectedItems: any) {
+    console.log(selectedItems);
+  }
+  getMultiselectValue(selectedItems: any) {
+    console.log(selectedItems);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { VnlpButtonModule } from '@em-and-ai/ui-sdk/vnlp-button';
+import { VnlpLabelTagModule } from '@em-and-ai/ui-sdk/vnlp-label-tag';
+import { VnlpCreateVirtualAgentModule } from '@em-and-ai/ui-sdk/vnlp-create-virtual-agent';
+import { VnlpTableModule } from '@em-and-ai/ui-sdk/vnlp-table';
+import { VnlpLogoModule } from '@em-and-ai/ui-sdk/vnlp-logo';
+import { VnlpSwitchModule } from '@em-and-ai/ui-sdk/vnlp-switch';
+import { VnlpCheckboxListModule } from '@em-and-ai/ui-sdk/vnlp-checkbox-list';
+import { VnlpTextareaModule } from '@em-and-ai/ui-sdk/vnlp-textarea';
+import { VnlpRadioCardModule } from '@em-and-ai/ui-sdk/vnlp-radio-card';
+import { VnlpTickboxModule } from '@em-and-ai/ui-sdk/vnlp-tickbox';
+import { VnlpSwitcherModule } from '@em-and-ai/ui-sdk/vnlp-switcher';
+import { VnlpInputTextModule } from '@em-and-ai/ui-sdk/vnlp-input-text';
+import { VnlpCheckboxModule } from '@em-and-ai/ui-sdk/vnlp-checkbox';
+import { VnlpPopoverModule } from '@em-and-ai/ui-sdk/vnlp-popover';
+import { VnlpFileUploaderModule } from '@em-and-ai/ui-sdk/vnlp-file-uploader';
+import { VnlpCalendarModule } from '@em-and-ai/ui-sdk/vnlp-calendar';
+import { VnlpStepBarModule } from '@em-and-ai/ui-sdk/vnlp-step-bar';
+import { VnlpHeaderModule } from '@em-and-ai/ui-sdk/vnlp-header';
+import { VnlpSuvNavVerticalModule } from '@em-and-ai/ui-sdk/vnlp-suv-nav-vertical';
+import { VnlpAvatarModule } from '@em-and-ai/ui-sdk/vnlp-avatar';
+import { VnlpSearchModule } from '@em-and-ai/ui-sdk/vnlp-search';
+import { VnlpDropdownModule } from '@em-and-ai/ui-sdk/vnlp-dropdown';
+import { VnlpMultiselectModule } from '@em-and-ai/ui-sdk/vnlp-multiselect';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    VnlpButtonModule,
+    VnlpCheckboxListModule,
+    VnlpLabelTagModule,
+    VnlpSwitchModule,
+    VnlpTextareaModule,
+    VnlpTableModule,
+    VnlpCreateVirtualAgentModule,
+    VnlpCheckboxModule,
+    VnlpTickboxModule,
+    VnlpInputTextModule,
+    VnlpLogoModule,
+    VnlpSwitcherModule,
+    VnlpRadioCardModule,
+    VnlpPopoverModule,
+    VnlpFileUploaderModule,
+    VnlpCalendarModule,
+    VnlpStepBarModule,
+    VnlpHeaderModule,
+    VnlpSuvNavVerticalModule,
+    VnlpAvatarModule,
+    VnlpSearchModule,
+    VnlpDropdownModule,
+    VnlpMultiselectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
